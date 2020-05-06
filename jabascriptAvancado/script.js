dd39d4a15b66d597420bf903056363fce468a85b
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-05-05 21:06:50 -0300

    Javascript advanced

--- jabascriptAvancado/script.js
+++ jabascriptAvancado/script.js
@@ -29,7 +29,7 @@ functionBind()
 */
 
 //Arrow
-/* Função comm apenas um parametro 
+/* Função comm apenas um parametro
 let dobro = x => 2 * x
 
 console.log(dobro(10))
@@ -65,10 +65,10 @@ listUsers(add,"Laercio")
 let listOfUsers = ["Ederson"]
 
 function add (user){
-  
+
   let promise = new Promise(function(resolve, reject){
 
-    
+
     setTimeout(()=>{
       listOfUsers.push(user)
       let error = false
@@ -82,15 +82,15 @@ function add (user){
     },1000)
 
   })
-  
+
   return promise
 
 }
 
 function listUsers(){
-  
+
   console.log(listOfUsers)
-  
+
 }
 
 add("Laercio")
@@ -103,10 +103,10 @@ add("Laercio")
 let listOfUsers = ["Ederson"]
 
 function add (user){
-  
+
   let promise = new Promise(function(resolve, reject){
 
-    
+
     setTimeout(()=>{
       listOfUsers.push(user)
       let error = false
@@ -120,15 +120,15 @@ function add (user){
     },1000)
 
   })
-  
+
   return promise
 
 }
 
 function listUsers(){
-  
+
   console.log(listOfUsers)
-  
+
 }
 
 async function assimEmaisFacil(){
@@ -140,6 +140,7 @@ assimEmaisFacil()
 */
 
 //Filter
+/*
 function criarAluno(nome,idade){
   return{nome,idade}
 }
@@ -154,3 +155,136 @@ function menor(pessoas){
   return pessoas.idade<30
 }
 console.log(pessoas.filter(menor))
+*/
+
+//Map
+/*
+function criarAluno(nome, idade) {
+  return { nome, idade }
+}
+
+let pessoas = [
+  criarAluno("Laercio", 54),
+  criarAluno("Maria", 55),
+  criarAluno("Ederson", 24),
+  criarAluno("Rafaela", 25)
+]
+
+function pessoasMaisVelhas(pessoas) {
+  return criarAluno(pessoas.nome, pessoas.idade + 5)
+}
+
+let novoArray = pessoas.map(pessoasMaisVelhas)
+*/
+
+//Reduce
+/*
+function criarAluno(nome, idade) {
+  return { nome, idade }
+}
+
+let pessoas = [
+  criarAluno("Laercio", 54),
+  criarAluno("Maria", 55),
+  criarAluno("Ederson", 24),
+  criarAluno("Rafaela", 25)
+]
+
+let todosNomes = (total, pessoas) => {
+  return total + pessoas.nome + ", "
+}
+
+console.log(pessoas.reduce(todosNomes, ""))
+*/
+
+// Desafio
+/*
+function criarAluno(nome, idade) {
+  return { nome, idade }
+}
+
+let pessoas = [
+  criarAluno("Laercio", 54),
+  criarAluno("Maria", 55),
+  criarAluno("Ederson", 24),
+  criarAluno("Rafaela", 25)
+]
+
+function pessoasMaisVelhas(pessoas) {
+  return criarAluno(pessoas.nome, pessoas.idade + 5)
+}
+
+let novoArrayUsandoMap = pessoas.map(pessoasMaisVelhas)
+
+let novoArrayDesafio = []
+let desafio = function desafio(pessoas) {
+  for (pessoa of pessoas) {
+    let arr = {
+      nome: pessoa.nome,
+      idade: pessoa.idade + 5
+    }
+    novoArrayDesafio.push(arr)
+  }
+  return novoArrayDesafio
+}
+
+console.log(novoArrayUsandoMap)
+console.log(desafio(pessoas))
+
+let todosNomes = (total, pessoas) => {
+  return total + pessoas.idade
+}
+
+console.log(pessoas.reduce(todosNomes, 0))
+
+function desafioReduce(pessoas) {
+  let todos = 0
+  for (pessoa of pessoas) {
+    todos += pessoa.idade
+  }
+
+  console.log(todos)
+}
+
+desafioReduce(pessoas)
+*/
+
+//Fetch
+/*
+let url = "https://api.exchangeratesapi.io/latest?base=USD&symbols=BRL"
+let valorDolar
+fetch(url)
+  .then((resp) => {
+    return resp.json()
+  })
+  .then((date) => {
+    valorDolar = date.rates.BRL
+    console.log(valorDolar)
+  })
+
+
+
+function buscarCEP() {
+  let digitaCEP = document.getElementById("CEP")
+  let CEP = digitaCEP.value
+  let p = document.querySelector("p")
+
+  let urlCorreio = `https://viacep.com.br/ws/${CEP}/json/`
+
+  fetch(urlCorreio)
+    .then(resp => resp.json())
+    .then(d => {
+      let rua = d.logradouro
+      let cidade = d.localidade
+      let bairro = d.bairro
+      let estado = d.uf
+      let cep = d.cep
+
+      p.innerHTML = `<p> rua: ${rua} </p>
+                      <p> cidade: ${cidade} </p>
+                      <p> bairro: ${bairro} </p>
+                      <p> estado: ${estado} </p>
+                      <p> cep: ${cep} </p>`
+    })
+}
+*/
