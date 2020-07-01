ca0c98d05bcc7c3b4f9893352880897d5f9ecb74
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-07-01 20:48:56 -0300

    for and if and parsials in EJS engine template

--- NodeJS/node_EJS/index.js
+++ NodeJS/node_EJS/index.js
@@ -3,16 +3,33 @@ import fs from "fs";
 import path from "path";
 const app = express();
 
-const user = {
-  name: "Ederson",
-  phone: "11 - 123456789",
-};
+const users = [
+  {
+    id: 0,
+    name: "Ederson",
+    phone: "11 - 123456789",
+  },
+  {
+    id: 1,
+    name: "Ederson",
+    phone: "11 - 123456789",
+  },
+  {
+    id: 2,
+    name: "Ederson",
+    phone: "11 - 123456789",
+  },
+];
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", user);
+  res.render("index", { users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(5000, () => {
