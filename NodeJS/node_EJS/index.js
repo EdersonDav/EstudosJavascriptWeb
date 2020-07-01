c29adcc385079cd17b293921311577bc05303e4e
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-07-01 19:47:12 -0300

    Template Engine EJS

---
+++ NodeJS/node_EJS/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+const app = express();
+
+const user = {
+  name: "Ederson",
+  phone: "11 - 123456789",
+};
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", user);
+});
+
+app.listen(5000, () => {
+  console.log("Server Runing Port 5000");
+});
